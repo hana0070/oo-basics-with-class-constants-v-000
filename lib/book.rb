4d487502358d96a77bc210af192e5aa7314812db
HANA LEANDER <hana0070@gmail.com>
2019-04-30 17:39:57 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,10 +3,14 @@ class Book
   attr_reader :title
 
   GENRES = []
-  
+
   def initialize(title)
     @title = title
   end
+
+  def genre=(genre)
+    @genre = genre
+  end 
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
